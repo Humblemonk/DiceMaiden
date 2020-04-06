6122e3a3cd67d4a730089465fc4cbc76f0f52470
Humblemonk <humblemonk@gmail.com>
2020-04-05 22:49:33 -0700

    Update dice_maiden_init.rb

--- dice_maiden_init.rb
+++ dice_maiden_init.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 # simple init script for dice maiden shards
 # Due to how long discordrb takes to start, starting multiple shards at once can cause them to hang
-# This script spaces the shard inits out every minute to help prevent this
+# This script spaces the shard inits out to help prevent this
 
 i = 0
 
