e5842a1542bb2d8963e4add1df94d5bece5c6041
Humblemonk <humblemonk@gmail.com>
2020-04-09 14:44:41 -0700

    Restructing folders and files
    Moved a few scripts to a folder to help clean up the root directory

---
+++ scripts/dice_maiden_quota.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+# Check current session start quota. Helpful to check how close the API toke is from being reset by Discord
+
+require 'dotenv'
+
+Dotenv.load
+token = ENV['TOKEN']
+exec("curl -s -H \"Authorization: Bot #{token}\" https://discordapp.com/api/v6/gateway/bot | jq")
