7e355dab42a7bd28691281f07bd61c6d32a79c84
ThatGuyWhatsMadeofCheese <69303484+ThatGuyWhatsMadeofCheese@users.noreply.github.com>
2021-01-26 02:01:39 -0600

    Added Hero System support

--- dice_maiden.rb
+++ dice_maiden.rb
@@ -124,9 +124,14 @@ mutex = Mutex.new
       else
         next if do_roll(event) == true
       end
-
+            
       # Output aliasing
       @tally = alias_output_pass(@tally)
+      
+      # Does calculations for Hero System stuff, if necessary
+      if @hsn || @hsk
+        hero_system_math
+      end
 
       # Grab event user name, server name and timestamp for roll and log it
       if @launch_option == "debug"
