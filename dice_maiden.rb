308e4c2b0a810fe39764f3e35c7b570c095aee51
Humblemonk <humblemonk@gmail.com>
2021-04-25 10:45:21 -0400

    Update dice_maiden.rb

--- dice_maiden.rb
+++ dice_maiden.rb
@@ -1,6 +1,6 @@
 # Dice bot for Discord
 # Author: Humblemonk
-# Version: 6.8.1
+# Version: 7.0.0
 # Copyright (c) 2017. All rights reserved.
 # !/usr/bin/ruby
 # If you wish to run a single instance of this bot, please follow the "Manual Install" section of the readme!
