cdeaf17fe6231e658f8621b2bab561c4cea95718
Dean Petty <dean.petty@gmail.com>
2023-05-08 12:11:00 -0500

    Update earthdawn_logic.rb
    Changed some display pieces to make sure they are showing what dice are being rolled. My players specifically keep thinking its wrong but i think if we actually show what dice are being thrown people will trust it more. Also if it turns out to be wrong some day, it will make it infinitely easier to report it.

--- earthdawn_logic.rb
+++ earthdawn_logic.rb
@@ -101,7 +101,7 @@ def replace_earthdawn(event)
   roll = @input.match(/^\s*ed(\d+)/i)
   step = roll[1].to_i
   if step.between?(1, 50)
-    @input.sub!('ed' + roll[1], @earthdawn_replacements[step])
+    @input.sub!('ed ('+@earthdawn_replacements[step]+')' + roll[1], @earthdawn_replacements[step])
   else
     event.respond(content: 'Only steps 1-50 are implemented')
     false
@@ -112,7 +112,7 @@ def replace_earthdawn4(event)
   roll = @input.match(/^\s*ed(\d+)/i)
   step = roll[1].to_i
   if step.between?(1, 40)
-    @input.sub!('ed' + roll[1], @earthdawn4_replacements[step])
+    @input.sub!('ed4e ('+@earthdawn4_replacements[step]+')' + roll[1], @earthdawn4_replacements[step])
   else
     event.respond(content: 'Only steps 1-40 are implemented')
     false
